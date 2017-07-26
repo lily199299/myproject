7faf50e258dc8deb43e620ab3ef8dfbc9047f990
王亚蕾 <13521682337@163.com>
2017-07-26 15:04:11 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+    <h1>{{ title }}</h1>
+    <input v-model="newItem" v-on:keyup.enter="addItem">
+    <ul v-for="item in items">
+      <li v-bind:class="{finished : item.isFinished}" v-on:click="toggleFun(item)">{{ item.label }}</li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data () {
+      return {
+        title: 'this is a app',
+        items: [],
+        newItem: ''
+      }
+    },
+    methods: {
+      toggleFun: function (item) {
+        item.isFinished = !item.isFinished
+      },
+      addItem: function () {
+        console.log(this.newItem)
+        this.items.push({
+          label: this.newItem,
+          isFinished: false
+        })
+        this.newItem = ''
+      }
+    }
+  }
+</script>
+
+<style>
+  .finished {
+    text-decoration: underline;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
